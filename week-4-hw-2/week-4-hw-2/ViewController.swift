4c6bbe22aa74fa09470727cb3341f5bd570098e3
Mahmut Yazar <mahmutyazarios@gmail.com>
2022-12-29 13:06:38 +0300

    Project Created

---
+++ week-4-hw-2/week-4-hw-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-4-hw-2
+//
+//  Created by Mahmut Yazar on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
